31b32caa9f618ffc523f373f2c3d095cf2bbd1dd
yudistiraAshadi <distra96@yahoo.com>
2016-12-01 21:53:06 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+package touristAssistant;
+
+import java.util.*;
+
+import databaseHandler.PlaceDataProcessing;
+import databaseHandler.UserDataProcessing;
+import personalSchedule.Event;
+import personalSchedule.Reminder;
+import place.Place;
+import user.User;
+import userInterface.ScheduleMenu;
+import userInterface.UI;
+import userInterface.UserAuthentication;
+
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println( "\t TOURIST ATTRACTION \n" );
+        UserAuthentication.LoginMenu();
+       // ScheduleMenu.setTime();
+    }
+    
+    
+   
+    
+ 
+}
+
